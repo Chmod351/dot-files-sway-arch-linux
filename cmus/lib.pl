8571ee1ae452cc5417e80596ea824dc58ab891d6
Chmod351 <yamil.t.351@proton.me>
2026-03-28 15:45:19 -0300

    add cmus config

---
+++ cmus/lib.pl
@@ -0,0 +1,156 @@
+/mnt/data1/Música/[1 Hour] CYBERPUNK METAL V1 🎸🔥Background Music for Gaming, Streaming and Working out! 💪🔥.mp3
+/mnt/data1/Música/5ml_per_second_irritation.mp3
+/mnt/data1/Música/5ml_per_secon_kanae_feelings.mp3
+/mnt/data1/Música/ahora-o-nunca.mp3
+/mnt/data1/Música/album.mp3
+/mnt/data1/Música/all-i-need.mp3
+/mnt/data1/Música/amor-eterno.mp3
+/mnt/data1/Música/anonimo.mp3
+/mnt/data1/Música/antonio-vivaldi-summer-full-the-four-seasons.mp3
+/mnt/data1/Música/Arthas, My Son - World of Warcraft Música (Letra y Traducción).mp3
+/mnt/data1/Música/aun_te_amo.mp3
+/mnt/data1/Música/avanti-morocha.mp3
+/mnt/data1/Música/bedshaped.mp3
+/mnt/data1/Música/before-i-forget.mp3
+/mnt/data1/Música/blowers-daughter.mp3
+/mnt/data1/Música/bon-jovi-bed-of-roses-official-music-video.mp3
+/mnt/data1/Música/BREATH OF LIFE Full Album Composed by Dustin Krizan EpicMusicVN.mp3
+/mnt/data1/Música/breath-of-life-youtube.mp3
+/mnt/data1/Música/bugatti.mp3
+/mnt/data1/Música/call-to-arms.mp3
+/mnt/data1/Música/capone.mp3
+/mnt/data1/Música/Céline Dion - The Power Of Love (Official Remastered HD Video).mp3
+/mnt/data1/Música/chamillionaire-ridin.mp3
+/mnt/data1/Música/clann-i-hold-you.mp3
+/mnt/data1/Música/cold.mp3
+/mnt/data1/Música/coldplay.mp3
+/mnt/data1/Música/como-seria.mp3
+/mnt/data1/Música/comp.mp3
+/mnt/data1/Música/conchita-incendios-ft-nach.mp3
+/mnt/data1/Música/conga_fury.mp3
+/mnt/data1/Música/corporation.mp3
+/mnt/data1/Música/cruza-en-rojo.mp3
+/mnt/data1/Música/cyberpunk_revenge.mp3
+/mnt/data1/Música/desde-ahora.mp3
+/mnt/data1/Música/despues-de-ti-alejandro-lerner-letra-wmv.mp3
+/mnt/data1/Música/diez-años-despues.mp3
+/mnt/data1/Música/DJ+Sammy+feat.+Yanou+&+Do+-+Heaven+-+Candlelight+Mix+(Official+Video).mp3
+/mnt/data1/Música/down.mp3
+/mnt/data1/Música/dulce-otoño.mp3
+/mnt/data1/Música/eligeme.mp3
+/mnt/data1/Música/ella.mp3
+/mnt/data1/Música/El Pájaro Vio el Cielo y Se Voló.mp3
+/mnt/data1/Música/epic_music_merethe.mp3
+/mnt/data1/Música/eres-mi-religion.mp3
+/mnt/data1/Música/false-king.mp3
+/mnt/data1/Música/fantasy-music-lumina.mp3
+/mnt/data1/Música/fate.mp3
+/mnt/data1/Música/final_hope.mp3
+/mnt/data1/Música/from-this-moment.mp3
+/mnt/data1/Música/fuckme.mp3
+/mnt/data1/Música/god.mp3
+/mnt/data1/Música/groost_terror.mp3
+/mnt/data1/Música/helena.mp3
+/mnt/data1/Música/Hellblade II Oficial Sountrack-Heilung  In Maidjan.mp3
+/mnt/data1/Música/Hellblade II Oficial Sountrack-Heilung  In Maidjan.webm
+/mnt/data1/Música/hoobastank_the_reasson.mp3
+/mnt/data1/Música/hospital_for_souls.mp3
+/mnt/data1/Música/hoy-no-estas.mp3
+/mnt/data1/Música/humanimal.mp3
+/mnt/data1/Música/im-in-love.mp3
+/mnt/data1/Música/im-trapped-in-you.mp3
+/mnt/data1/Música/invincible.mp3
+/mnt/data1/Música/in-your-past.mp3
+/mnt/data1/Música/i_see_you.mp3
+/mnt/data1/Música/ivan torrent crystalline.mp3
+/mnt/data1/Música/kimimaro.mp3
+/mnt/data1/Música/knights_and_lords.mp3
+/mnt/data1/Música/lagrimas.mp3
+/mnt/data1/Música/la-mujer-que-no-soñe.mp3
+/mnt/data1/Música/lastima.mp3
+/mnt/data1/Música/legacy_of_the_lost.mp3
+/mnt/data1/Música/linger.mp3
+/mnt/data1/Música/lissa-gerrard.mp3
+/mnt/data1/Música/lost_mattia_cupelli.mp3
+/mnt/data1/Música/low_of_solipsium.mp3
+/mnt/data1/Música/mana-bendita-tu-luz-video-oficial.mp3
+/mnt/data1/Música/mariah-carey-hero-official-lyric-video.mp3
+/mnt/data1/Música/mariah-carey-without-you-official-lyric-video.mp3
+/mnt/data1/Música/Mattia Cupelli - Oceano (Il Cacciatore Di Arcobaleni).mp3
+/mnt/data1/Música/mi-credo-tiziano-ferro-y-pepe-aguilar.mp3
+/mnt/data1/Música/mi-luna.mp3
+/mnt/data1/Música/mix_dark_techno.mp3
+/mnt/data1/Música/never-cry-again.mp3
+/mnt/data1/Música/ni_estabas_ni_estaras.mp3
+/mnt/data1/Música/nightfall.mp3
+/mnt/data1/Música/no-hace-falta.mp3
+/mnt/data1/Música/no_puedo_dejarte_de_amar.mp3
+/mnt/data1/Música/ojala-pudiera-borrarte.mp3
+/mnt/data1/Música/palabras_para_paula.mp3
+/mnt/data1/Música/penelope.mp3
+/mnt/data1/Música/please-forgive-me.mp3
+/mnt/data1/Música/por-amarte-asi.mp3
+/mnt/data1/Música/princesa.mp3
+/mnt/data1/Música/programing0.mp3
+/mnt/data1/Música/programing1.mp3
+/mnt/data1/Música/programing2.mp3
+/mnt/data1/Música/programing3.mp3
+/mnt/data1/Música/qillix_most_wanted.mp3
+/mnt/data1/Música/ramstein.mp3
+/mnt/data1/Música/reckless-and-relentless.mp3
+/mnt/data1/Música/requiem_for_a_dream.mp3
+/mnt/data1/Música/richard_marx.mp3
+/mnt/data1/Música/room-of-angel.mp3
+/mnt/data1/Música/Roxette+-+Spending+My+Time.mp3
+/mnt/data1/Música/ruido-blanco.mp3
+/mnt/data1/Música/sad.mp3
+/mnt/data1/Música/sao.mp3
+/mnt/data1/Música/sao_soundtracks.mp3
+/mnt/data1/Música/saw2.mp3
+/mnt/data1/Música/saw.mp3
+/mnt/data1/Música/sentada-aqui-en-mi-alma.mp3
+/mnt/data1/Música/si-tu-no-vuelves.mp3
+/mnt/data1/Música/snuff.mp3
+/mnt/data1/Música/somewhere-over-the-rainbow.mp3
+/mnt/data1/Música/springs_mattia_cupelli.mp3
+/mnt/data1/Música/sunday.mp3
+/mnt/data1/Música/synestesia.mp3
+/mnt/data1/Música/Tainted Grail_ The Fall of Avalon Druid Chant-74pCu7wMaO8.webm
+/mnt/data1/Música/tartini-violin-sonata-in-g-minor-devils-trill-sonata.mp3
+/mnt/data1/Música/te-extraño.mp3
+/mnt/data1/Música/tell-me-anything.mp3
+/mnt/data1/Música/tender_feeling.mp3
+/mnt/data1/Música/thank-you-for-loving-me.mp3
+/mnt/data1/Música/the_last_butterfly.mp3
+/mnt/data1/Música/the_truth.mp3
+/mnt/data1/Música/thomas-berge-eart-extended-youtube.mp3
+/mnt/data1/Música/time.mp3
+/mnt/data1/Música/tiny_love.mp3
+/mnt/data1/Música/to-stage.mp3
+/mnt/data1/Música/two-steps-fr-ended-version-youtube.mp3
+/mnt/data1/Música/ultima-llamada.mp3
+/mnt/data1/Música/u-r-always.mp3
+/mnt/data1/Música/vacio.mp3
+/mnt/data1/Música/velatorio.mp3
+/mnt/data1/Música/vivo_per_lei.mp3
+/mnt/data1/Música/volver_a_nacer.mp3
+/mnt/data1/Música/watch_the_world_burn.mp3
+/mnt/data1/Música/when-youre-gone.mp3
+/mnt/data1/Música/who_am_i.mp3
+/mnt/data1/Música/wild-hearth-thomas-begersen (Copy).mp3
+/mnt/data1/Música/wild-hearth-thomas-begersen.webm
+/mnt/data1/Música/will-there-be-light-youtube.mp3
+/mnt/data1/Música/winterwish_fixed.mp3
+/mnt/data1/Música/winterwish.mp3
+/mnt/data1/Música/wod.mp3
+/mnt/data1/Música/World of Warcraft - Nightsong Extended.mp3
+/mnt/data1/Música/World Of Warcraft Soundtrack - Stormwind (City Theme).mp3
+/mnt/data1/Música/yiruma-love-me.mp3
+/mnt/data1/Música/yo-queria.mp3
+/mnt/data1/Música/todoke_kanai_kimochi.mp3
+/mnt/data1/Música/death.mp3
+/mnt/data1/Música/Akira Takemoto - Track 44.mp3
+/mnt/data1/Música/yt1z.net - Luis Fonsi - Se Supone (320 KBps).mp3
+/mnt/data1/Música/o_fortuna.mp3
+/mnt/data1/Música/shingeki.mp3
+/mnt/data1/Música/1994 Hacked successfully Synthwave, Retrowave, Outrun, Cyberpunk, Chillwave.mp3
